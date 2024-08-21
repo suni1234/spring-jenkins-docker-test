9a58e8e7c2efe74c78ae25ae596a14f7a3fc29b5
suni1234 <sunilkumar.kakani11@gmail.com>
2024-08-21 12:12:29 +0530

    first commit

---
+++ src/test/java/com/learn/jenkins/docker/api/SpringJenkinsDockerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.jenkins.docker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsDockerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
