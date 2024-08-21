2f8e406a9186ca4080c1cb621e59d342038bc2cb
suni1234 <sunilkumar.kakani11@gmail.com>
2024-08-21 13:02:51 +0530

    second commit

--- src/test/java/com/learn/jenkins/docker/api/SpringJenkinsDockerTestApplicationTests.java
+++ src/test/java/com/learn/jenkins/docker/api/SpringJenkinsDockerTestApplicationTests.java
@@ -1,13 +1,21 @@
 package com.learn.jenkins.docker.api;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringJenkinsDockerTestApplicationTests {
 
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsDockerTestApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		logger.info("Test case execusting");
+		logger.info("Test case execustingthiredstatement");
+		assertEquals(true, true);
 	}
-
 }
